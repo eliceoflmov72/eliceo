e089196bc1cdaea49f44f108bce8b7cd29d6e175
eliceoflmov72 <eliceomoreta@gmail.com>
2026-02-23 21:36:35 +0100

    feat: new info

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -3,16 +3,15 @@ import { Component } from '@angular/core';
 
 import { RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TitleComponent } from '../shared/title/title.component';
+import { AnimationDirective } from '../shared/directives/animation.directive';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [
-    RouterLink,
-    TranslateModule
-],
+  imports: [RouterLink, TranslateModule, TitleComponent, AnimationDirective],
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
+  styleUrls: ['./info.component.css'],
 })
 export class InfoComponent {
   constructor(public translate: TranslateService) {}
